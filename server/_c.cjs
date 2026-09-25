5f22c0073cf68b2dc898a06be452957d22963a61
jaswant <jdunga@gitam.in>
2026-09-25 17:46:29 +0530

    merged features from vithesh to jaswant

--- server/_c.cjs
+++
@@ -1,2 +0,0 @@
-const { PrismaClient } = require('@prisma/client'); const p = new PrismaClient();
-p.user.count({ where: { email: 'not.on.the.list@gmail.com' } }).then(n => { console.log(n === 0 ? 'no row created (good)' : 'ROW CREATED (bad)'); return p.$disconnect(); });
